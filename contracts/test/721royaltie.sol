60eadbd201e1917d7099734d5dbe3cae320cb248
wolffhack <69741055+wolffhack@users.noreply.github.com>
2022-07-10 18:35:39 -0500

    new Contract

--- contracts/test/721royaltie.sol
+++ contracts/test/721royaltie.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import {IERC165, ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
 contract ktreeNFT is ERC721, IERC2981 {
